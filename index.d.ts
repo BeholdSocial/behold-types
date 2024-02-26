3979fac9e93e0ed6ea492ec87f4110b2915b445c
Griffin <griffinjohnston@gmail.com>
2024-02-26 09:22:51 -0500

    +namespace

--- index.d.ts
+++ index.d.ts
@@ -1,61 +1,63 @@
-interface Feed {
-  username: string
-  biography: string
-  profilePictureUrl: string
-  website: string
-  followersCount: number
-  followsCount: number
-  hashtags?: string[]
-  posts: Post[]
-}
+declare namespace Behold {
+  export interface Feed {
+    username: string
+    biography: string
+    profilePictureUrl: string
+    website: string
+    followersCount: number
+    followsCount: number
+    hashtags?: string[]
+    posts: Post[]
+  }
 
-interface Post {
-  id: string
-  timestamp: string
-  permalink: string
-  mediaType: "IMAGE" | "VIDEO" | "CAROUSEL_ALBUM"
-  isReel?: boolean
-  mediaUrl: string
-  thumbnailUrl?: string
-  sizes: PostSizes
-  caption: string
-  prunedCaption: string
-  hashtags: string[]
-  hashtag?: string
-  mentions: string[]
-  colorPalette: ColorPalette
-  children?: ChildPost[]
-}
+  export interface Post {
+    id: string
+    timestamp: string
+    permalink: string
+    mediaType: "IMAGE" | "VIDEO" | "CAROUSEL_ALBUM"
+    isReel?: boolean
+    mediaUrl: string
+    thumbnailUrl?: string
+    sizes: PostSizes
+    caption: string
+    prunedCaption: string
+    hashtags: string[]
+    hashtag?: string
+    mentions: string[]
+    colorPalette: ColorPalette
+    children?: ChildPost[]
+  }
 
-interface ChildPost {
-  id: string
-  mediaType: string
-  mediaUrl: string
-  sizes: PostSizes
-  colorPalette: ColorPalette
-}
+  export interface ChildPost {
+    id: string
+    mediaType: string
+    mediaUrl: string
+    sizes: PostSizes
+    colorPalette: ColorPalette
+  }
 
-interface PostSizes {
-  small: PostSize
-  medium: PostSize
-  large: PostSize
-  full: PostSize
-}
+  export interface PostSizes {
+    small: PostSize
+    medium: PostSize
+    large: PostSize
+    full: PostSize
+  }
 
-interface PostSize {
-  height: number
-  width: number
-  mediaUrl: string
-}
+  export interface PostSize {
+    height: number
+    width: number
+    mediaUrl: string
+  }
 
-interface ColorPalette {
-  dominant: RGBString
-  muted: RGBString
-  mutedLight: RGBString
-  mutedDark: RGBString
-  vibrant: RGBString
-  vibrantLight: RGBString
-  vibrantDark: RGBString
-}
+  export interface ColorPalette {
+    dominant: RGBString
+    muted: RGBString
+    mutedLight: RGBString
+    mutedDark: RGBString
+    vibrant: RGBString
+    vibrantLight: RGBString
+    vibrantDark: RGBString
+  }
 
-type RGBString = `${number}, ${number}, ${number}`
+  export type RGBString = `${number}, ${number}, ${number}`
+}
